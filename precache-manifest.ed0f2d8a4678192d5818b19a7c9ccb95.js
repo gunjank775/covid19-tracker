329a73e76203829b2b9393c9b3c32a83971132ff
gunjank775 <49212572+gunjank775@users.noreply.github.com>
2020-05-15 22:26:56 +0530

    Updates

---
+++ precache-manifest.ed0f2d8a4678192d5818b19a7c9ccb95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b5a04f192f4cc80dcb0d161c02ee05",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "e35bf980140d68166254",
+    "url": "/covid19-tracker/static/css/main.1fbaa1b9.chunk.css"
+  },
+  {
+    "revision": "c07711f0f34e233ae7b9",
+    "url": "/covid19-tracker/static/js/2.e8e0ef59.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.e8e0ef59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35bf980140d68166254",
+    "url": "/covid19-tracker/static/js/main.091c3337.chunk.js"
+  },
+  {
+    "revision": "6ea81e9ada301f10b8bb",
+    "url": "/covid19-tracker/static/js/runtime-main.59e62a6a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
